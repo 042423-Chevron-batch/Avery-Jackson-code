d2dc2c0962646d623653911c0fb80187739fc5e7
Avery Jackson <averyjackson@averys-air.attlocal.net>
2023-04-28 12:12:43 -0500

    Updated user input

--- Week1Project/carwash.ps1
+++ Week1Project/carwash.ps1
@@ -1,16 +1,15 @@
+$package = Read-Host "Which wash package would you like (Basic, Deluxe, or Premium)?"
 
 
-foreach($package in $args){
-    if($package -match 'basic'){
+    if($package -eq 'basic'){
         Write-Output "You have selected the basic wash. Your total is 7 dollars."
     }
-    elseif($package -match 'deluxe'){
+    elseif($package -eq 'deluxe'){
         Write-Output "You have selected the deluxe wash. Your total is 9 dollars."
     }
-    elseif($package -match 'premium'){
+    elseif($package -eq 'premium'){
         Write-Output "You have selected the Premium wash. Your total is 12 dollars."
     }
     else{
-        Write-Output "Invalid Entry"
+        Write-Output "Invalid Entry. Please select either basic, deluxe, or premium!"
     }
-}
