1b0d6c9bb831c106b9d4169f3e70b08ebdc69dbf
Avery Jackson <averyjackson@averys-air.attlocal.net>
2023-05-01 09:53:26 -0500

    Final code

--- Week1Project/carwash.ps1
+++ Week1Project/carwash.ps1
@@ -3,13 +3,21 @@ $package = Read-Host "Which wash package would you like (Basic, Deluxe, or Premi
 
     if($package -eq 'basic'){
         Write-Output "You have selected the basic wash. Your total is 7 dollars."
+       
+        
     }
     elseif($package -eq 'deluxe'){
         Write-Output "You have selected the deluxe wash. Your total is 9 dollars."
+        
     }
     elseif($package -eq 'premium'){
         Write-Output "You have selected the Premium wash. Your total is 12 dollars."
+        
     }
     else{
         Write-Output "Invalid Entry. Please select either basic, deluxe, or premium!"
     }
+$money = Read-Host " You may pay now. Please enter your money. Enter either 10 or 20 dollars."
+
+    Write-Output "Thank you, you may proceed for your wash. Please take your change."
+
